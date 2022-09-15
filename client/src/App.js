afdb2aeef9b8715b877bea469a2a7c0fa5a5e255
Amy Welch <amzwelch@gmail.com>
2022-09-15 14:27:41 +0100

    amy commit for presentation

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import HomePage from "./Components/Homepage/HomePage";
 import MyChatComponent from "./Components/MyChatComponent.js";
 import LoginForm from "./Components/LogInForm.js";
 import AddWalkForm from "./Components/AddWalkForm";
-import AllWalks from "./Components/Allwalks/AllWalks"
+import AllWalks from "./Components/Allwalks/AllWalks";
 import { Routes, Route, Link, Navigate } from "react-router-dom";
 import SignUpForm from "./Components/SignUpForm";
 import IndividualWalk from "./Components/IndividualWalk";
@@ -15,16 +15,7 @@ import Aboutus from "./Components/Aboutus/Aboutus";
 function App() {
   const [loggedIn, setLoggedIn] = useState(null);
   const [Walks, setWalks] = useState([]);
-  const [Users, setUsers] = useState([
-    { user_name: "bigboy,", user_Id: 2 },
-    { user_name: "Frank james", user_Id: 3 }, //here we have hard coded user to try the fix but here we should make a fetch get to retrieve the users from the table
-  ]);
-  //Also Frank james is probably a bad example as the spaces in the name wouldn't work in a url so you would need to split and join the name to make it work
-  //Finally, do we need to the user name field in the log in page? usually it is the email and password?
-
-  const dataToChatComponent = () => {
-    //send data to chatComponent using this function
-  };
+  const [Users, setUsers] = useState();
 
   const handleLoggedInData = (loggedInData) => {
     //function for getting data from child component SignUpForm
--- client/src/Components/Allwalks/AllWalks.js
+++ client/src/Components/Allwalks/AllWalks.js
@@ -1,15 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import Walk from '../Walk/Walk';
-import "./Allwalks.css"
+import React, { useEffect, useState } from "react";
+import Walk from "../walk/Walk";
+import "./Allwalks.css";
 
-export default function AllWalks({walks}) {
-
-
+export default function AllWalks({ walks }) {
   return (
-    <div className='allWalks'>
-    {walks.map((w) => (
-          <Walk key={w.id} walk={w}/>
-    ))};
+    <div className="allWalks">
+      {walks.map((w) => (
+        <Walk key={w.id} walk={w} />
+      ))}
+      ;
     </div>
   );
-};
+}
