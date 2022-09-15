4e1f558c7f3614da6e373c8cfc78940e2cc4cf76
Sara Arruga <saraa@MBP-de-Sara.lan>
2022-09-15 13:23:49 +0200

    conflicts

--- client/src/Components/HomePage.js
+++ client/src/Components/HomePage.js
@@ -27,6 +27,7 @@ return (
         <img
           className="img-fluid main-page-header-image"
           src="https://images.unsplash.com/photo-1605001011156-cbf0b0f67a51?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2574&q=80"
+          alt="dog-women"
         ></img>
       </div>
       <h1>🐶 Welcome to our dog walks app! 🐶</h1>
